c0e22fa8dc4fb7b921cc8c993c8902ed0f7d7bad
Ju.Reyms <ju.reyms@gmail.com>
2024-09-03 19:47:18 +0200

    icon + manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,9 +30,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'manifest', href: '/manifest.json' },
-
-
-
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
       ],
     
     },
